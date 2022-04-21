f45fd52285863b744c2f07e8b0cbfe61c1a43352
HRSHDG8 <hrshdg8@gmail.com>
2022-04-21 15:12:47 -0700

    cs160

--- src/main/java/edu/sdsu/cs160l/algorithm/search/BinarySearch.java
+++ src/main/java/edu/sdsu/cs160l/algorithm/search/BinarySearch.java
@@ -2,7 +2,15 @@ package edu.sdsu.cs160l.algorithm.search;
 
 import java.util.List;
 
+/**
+ * TODO implement binary search in lab with your lab partners - ( 2 points )
+ * refer to linear search as to checking equality
+ */
 public class BinarySearch implements Search{
+    /**
+     * Input to binary search will always be a sorted array.
+     * write an algorithm that runs in O(log(n)) time, where n is input size
+     */
     @Override
     public <T> int search(List<T> list, T t) {
         return -1;
